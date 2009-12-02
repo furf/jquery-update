0d3143353fb8a85ac93d47c116804b9f55b135db
David Furfero <furf@bartloaner.mlbam.mlb.org>
2009-12-02 11:23:33 -0500

    first

--- jquery.update.js
+++ jquery.update.js
@@ -32,23 +32,23 @@
 
     // @todo handle namespacing?
     // @todo use data.interval for interval configuration - use add when available
-		setup: function(data, ns) {
+    setup: function(data, ns) {
 
-		  var elem  = this,
-		      $elem = $(elem);
+      var elem  = this,
+          $elem = $(elem);
 
-		  if ($elem.is(TEXT_INPUT)) {
+      if ($elem.is(TEXT_INPUT)) {
 
-				$elem
-				  .bind(START, update.handleStart)
-				  .bind(STOP, update.handleStop);
-		  
-		  } else if ($elem.is(OTHER_INPUT)) {
+        $elem
+          .bind(START, update.handleStart)
+          .bind(STOP, update.handleStop);
+      
+      } else if ($elem.is(OTHER_INPUT)) {
 
-		    $elem
-		      .bind(CHANGE, update.compare);
-		  
-		  } else if ($elem.is(FORM)) {
+        $elem
+          .bind(CHANGE, update.compare);
+      
+      } else if ($elem.is(FORM)) {
 
         $elem.data(HANDLER_KEY, function (evt) {
           $.event.handle.apply(elem, arguments);
@@ -57,39 +57,39 @@
         // @todo move to .live() when focus and blur are natively supported
         update.children = $elem.find(ALL_INPUTS)
           .bind(UPDATE, $elem.data(HANDLER_KEY));
-		  }
-		},
+      }
+    },
 
     // @todo remove data in cleanup
     // @todo handle namespacing?
-		teardown: function(ns) {
+    teardown: function(ns) {
 
-		  var $elem = $(this);
+      var $elem = $(this);
 
       if ($elem.is(TEXT_INPUT)) {
 
-				$elem
-				  .unbind(START, update.handleStart)
-				  .unbind(STOP, update.handleStop);
-		  
-		  } else if ($elem.is(OTHER_INPUT)) {
+        $elem
+          .unbind(START, update.handleStart)
+          .unbind(STOP, update.handleStop);
+      
+      } else if ($elem.is(OTHER_INPUT)) {
 
-		    $elem
-		      .unbind(CHANGE, update.compare);
-		  
-		  } else if ($elem.is(FORM)) {
+        $elem
+          .unbind(CHANGE, update.compare);
+      
+      } else if ($elem.is(FORM)) {
 
         update.children
           .unbind(UPDATE, $elem.data(HANDLER_KEY));
-		  }
-		},
-		
-		compare: function (evt) {
+      }
+    },
+    
+    compare: function (evt) {
 
-		  var elem    = this,
-		      $elem   = $(elem),
-		      current = $elem.val(),
-		      stored  = $elem.data(STORAGE_KEY);
+      var elem    = this,
+          $elem   = $(elem),
+          current = $elem.val(),
+          stored  = $elem.data(STORAGE_KEY);
 
       if (current !== stored) {
         $elem.data(STORAGE_KEY, current);
@@ -98,9 +98,9 @@
           $.event.handle.apply(elem, arguments);
         }
       }
-		},
-		
-		handleStart: function (evt) {
+    },
+    
+    handleStart: function (evt) {
 
       var elem = this;
       
@@ -109,17 +109,17 @@
           update.compare.call(elem, evt);
         }, update.interval);
       }
-		},
-		
-		handleStop: function (evt) {
+    },
+    
+    handleStop: function (evt) {
       if (update.timer) {
         update.timer = clearInterval(update.timer);
       }
-		}
-	};
+    }
+  };
 
   $.fn.update = function (fn) {
-		return fn ? this.bind(UPDATE, fn) : this.trigger(UPDATE);
+    return fn ? this.bind(UPDATE, fn) : this.trigger(UPDATE);
   };
   
-})(jQuery);  	
+})(jQuery);
